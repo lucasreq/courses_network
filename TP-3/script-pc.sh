ebf467be111b1845c56144db0fae360e342989df
Requena <lucas.requena@ynov.com>
2019-10-24 10:45:49 +0200

    script pc qui marche

--- TP-3/script-pc.sh
+++ TP-3/script-pc.sh
@@ -1,32 +1,51 @@
 #!/bin/bash
 
-telnet 192.168.56.101 5000 <<EOF
+( echo ; echo ; echo -e 
+sleep 2
+echo "ip 10.3.10.16/24 10.3.10.254"
+sleep 2
+echo "set pcname PC1"
+sleep 1
+echo "save"
+sleep 1
+echo "quit"  ) | telnet 192.168.56.101 5001
+
+( echo ; echo ; echo -e 
+sleep 2
+echo "ip 10.3.20.2/24 10.3.20.254"
 sleep 3
-ip 10.3.10.16/24 10.3.10.254
-set pcname PC1
-save
-EOF
+echo "set pcname PC2"
+sleep 1
+echo "save"
+sleep 1
+echo "quit"  ) | telnet 192.168.56.101 5002
 
-telnet 192.168.56.101 5002 <<EOF
-ip 10.3.20.2/24 10.3.20.254
-set pcname PC2
-save
-EOF
+( echo ; echo ; echo -e 
+sleep 2
+echo "ip 10.3.20.3/24 10.3.20.254"
+sleep 3
+echo "set pcname PC3"
+sleep 1
+echo "save"
+sleep 1
+echo "quit"  ) | telnet 192.168.56.101 5016
 
-telnet 192.168.56.101 5011 <<EOF
-ip 10.3.20.3/24 10.3.20.254
-set pcname PC3
-save
-EOF
+( echo ; echo ; echo -e 
+sleep 2
+echo "ip 10.3.30.4/24 10.3.30.254"
+sleep 3
+echo "set pcname PC4"
+sleep 1
+echo "save"
+sleep 1
+echo "quit"  ) | telnet 192.168.56.101 5010
 
-telnet 192.168.56.101 5006 <<EOF
-ip 10.3.30.4/24 10.3.30.254
-set pcname PC4
-save
-EOF
-
-telnet 192.168.56.101 5008 <<EOF
-ip 10.3.40.1/24 10.3.40.254
-set pcname P1
-save
-EOF
+( echo ; echo ; echo -e 
+sleep 2
+echo "ip 10.3.40.1/24 10.3.40.254"
+sleep 3
+echo "set pcname P1"
+sleep 1
+echo "save"
+sleep 1
+echo "quit" ) | telnet 192.168.56.101 5013
